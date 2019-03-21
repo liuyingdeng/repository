379fe955f15163b788fd2f817c393d5687b91199
liuyingdeng <liu2291022701@163.com>
2019-03-21 20:20:43 +0800

    learn git

---
+++ ArrayList/src/cn/xupt/TestArrayList/SxtArrayList.java
@@ -0,0 +1,96 @@
+package cn.xupt.TestArrayList;
+
+public class SxtArrayList {
+	private Object[] elementDate;
+	private int size;
+	
+	public SxtArrayList() {
+		this (10);
+	}
+	
+	public SxtArrayList(int initialCapacity) {
+		if(initialCapacity<0) {
+			try {
+				throw new Exception();
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		elementDate =new Object[initialCapacity];
+	}
+	public int size() {
+		return size;
+	}
+	public boolean isEmpty() {
+		return size ==0;
+	}
+	public void add(Object obj) {
+		//数组扩容和数组拷贝
+		if(size==elementDate.length) {
+			Object[] newArray= new Object[size*2+1];
+			System.arraycopy(elementDate,0,newArray,0,elementDate.length);
+			elementDate=newArray;
+		}
+		elementDate[size++]=obj;
+	}
+	public Object get(int index) {
+		rangeCheck(index);
+		return elementDate[index];
+	}
+	public void remove(int index) {
+		rangeCheck(index);
+		int numMoved =size-index-1;
+		if(numMoved>0) {
+			System.arraycopy(elementDate,index+1,elementDate,index,numMoved);
+		}
+		elementDate[--size]=null;
+	}
+	public void remove(Object obj) {//删除指定元素
+		for(int i=0;i<size;i++) {
+			if(get(i).equals(obj)) {//底层调用是equals，而不是==。
+				remove(i);
+			}
+		}
+	}
+	public Object set(int index,Object obj) {
+		rangeCheck(index);
+		Object OldValue=elementDate[index];
+		elementDate[index]=obj;
+		return OldValue;
+	}
+	public void add(int index,Object obj) {
+		rangeCheck(index);
+		ensureCapacity();//数组扩容
+		System.arraycopy(elementDate,index,elementDate,index+1,size-index);
+		elementDate[index++]=obj;
+	}
+	private void ensureCapacity() {
+		if(size==elementDate.length) {
+			Object[] newArray= new Object[size*2+1];
+			System.arraycopy(elementDate,0,newArray,0,elementDate.length);
+			elementDate=newArray;
+			}
+	}
+	private void rangeCheck(int index) {
+		if(index<0||index>=size) {
+			try {
+				throw new Exception();
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	public static void main(String[] args) {
+		SxtArrayList list = new SxtArrayList(3);
+		list.add("333");
+		list.add("444");
+		list.add("5");
+		list.add("344433");
+		list.add("333");
+		list.add("333");
+		System.out.println(list.size()); 
+		list.remove("444");
+		System.out.println(list.size());
+	}
+	
+}
